2486bd414589d42a3abb3f23e5c3da5b6c9f33ed
lArc244785 <63329185+lArc244785@users.noreply.github.com>
2023-12-06 23:31:37 +0900

    재료 타입 추가, 조리도구 타입 추가, 레시피 데이터 추가
    ● 레시피 데이터 추가
      - Assets>>Create>>AssetDats>>RecipeElementInfo을 통해서 AssetData를 생성할 수 있습니다.

---
+++ Assets/02.Scripts/IngredientDatas/UtensilType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CopycatOverCooked.Datas
+{
+	public enum UtensilType
+	{
+		None =				-1,
+		ChoppingBoard,
+		Pot,
+	}
+}
