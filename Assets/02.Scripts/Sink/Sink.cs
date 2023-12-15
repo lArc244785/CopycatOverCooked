750273b9de0ade85d01c63ef87c5feac5e53d1ec
GongDoRi <dol3467@hotmail.com>
2023-12-15 11:23:40 +0900

    StartSink
    싱크

---
+++ Assets/02.Scripts/Sink/Sink.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sink : MonoBehaviour
+{
+    [SerializeField] private GameObject _platePrefab;
+
+    private float _washingTime = 3f;
+
