9cbef43b936aaa8f51dafb06672de4762d693001
GongDoRi <dol3467@hotmail.com>
2023-12-15 14:29:36 +0900

    12.15 / Sink & Trashcan

--- Assets/02.Scripts/Sink/Sink.cs
+++ Assets/02.Scripts/Sink/Sink.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class Sink : MonoBehaviour
@@ -7,4 +8,34 @@ public class Sink : MonoBehaviour
     [SerializeField] private GameObject _platePrefab;
 
     private float _washingTime = 3f;
+    public NetworkVariable<bool> _hasPlates = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkObject _plate;
 
+    [ServerRpc]
+    public void WashingPlateServerRPC()
+    {
+        _plate.Despawn();
+
+        /*
+         
+        Progress Bar
+        
+        */
+
+        GameObject cleanPlate = Instantiate(_platePrefab, Vector3.up,Quaternion.identity);
+        cleanPlate.GetComponent<NetworkObject>().Spawn(true);
+
+        _hasPlates.Value = false;
+    }
+
+    public void AddPlate(NetworkObject _dirtyPlate)
+    {
+        if(_hasPlates.Value == true)
+        {
+            return;
+        }
+
+        _plate = _dirtyPlate;
+        _hasPlates.Value = true;
+    }
+}
